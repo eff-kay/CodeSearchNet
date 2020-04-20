ef05bc112feaa5cdeb9d02ca979bcb342e8755d2
Eunbee Jang <jangeunb@login-4.server.mila.quebec>
2020-04-20 01:21:40 -0400

    run script updated

--- src/job.sh
+++ src/job.sh
@@ -5,11 +5,11 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:v100-32gb:2
 #SBATCH --mem=32G
-#SBATCH --time=48:00:00
-#SBATCH -o /network/home/jangeunb/CodeSearchNet/src/slurm-%j.out
+#SBATCH --time=96:00:00
+#SBATCH -o /network/home/jangeunb/slurm_non_strict-%j.out
 
 module load python/3.7
 
 pip install -r requirements.txt
 
-python train.py --model SelfAttentionModel
+python train.py --model SelfAttentionModel --max-num-epochs 30
