a6f25490383238841631c4f0ae5be76c7cd4ee79
Eunbee Jang <jangeunb@login-4.server.mila.quebec>
2020-04-20 00:54:04 -0400

    run script for slurm

---
+++ src/job2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#SBATCH --partition=unkillable
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:v100-32gb:2
+#SBATCH --mem=32G
+#SBATCH --time=96:00:00
+#SBATCH -o /network/home/jangeunb/slurm_strict-%j.out
+
+module load python/3.7
+
+pip install -r requirements.txt
+
+python train.py --model SelfAttentionModel --max-num-epochs 30
